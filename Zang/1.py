c9d32f49806b20122a54b1bc80df605ab528b230
zhangshengfei <zhangshengfei@ftrader.pro>
2021-06-24 10:11:20 +0800

    默认更改列表

--- Zang/1.py
+++ Zang/1.py
@@ -3,3 +3,4 @@ print('中文'.encode('utf-8'))
 print(b'ABC'.decode('ascii'))
 print(b'\xe4\xb8\xad\xe6\x96\x87'.decode('utf-8',errors='ignore'))
 len('jhfdsjkfkskk')#1111111111111111111111111111111
+len('ghjhgjkhgjkbhjkl')
